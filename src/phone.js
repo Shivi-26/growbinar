ac1ded4532673ab299be80b4dc227de9029dc559
shivi-26 <cseshivangi66@gmail.com>
2023-03-21 19:31:42 +0530

    Added  all files of task

---
+++ src/phone.js
@@ -0,0 +1,109 @@
+import React from 'react';
+import img4 from './images/img4.png'
+export default function phone() {
+    return (
+        <div>
+            <div className='bg'>
+            </div>
+            <div className='d-flex mt-5 container me-5'>
+                <div className='container mt-5'>
+                    <img src={img4} height="500" width="250" className='ms-5 img' alt="" />
+                </div>
+                <div className='box'>
+                    <h5><b>Live Events to engage with the community</b></h5>
+                    <p>Drive home the learning with live quizzes. Q&As and debates so that you feel more confident with your learnings</p>
+                    <p>-----------------------------------------------------------------------</p>
+                    <div className='mt-3'>
+                        <h5><b>Get More Career opportunities</b></h5>
+                        <p>Be a part of the community where people help you gain more opportunities whether job interview or freelance work.</p>
+                    </div>
+                </div>
+            </div>
+            <div className='mt-5 mx-auto'>
+                <h5 className='text-center mt-5'><b>Filter Mentors by your objective or Field</b></h5>
+                <button className='butt ms-5'>UI/UX</button>
+                <button className='butt'>Front-End Dev.</button>
+                <button className='butt'>Data Manage..</button>
+                <button className='butt'>Sales Ex.</button>
+                <button className='butt'>Product Mana..</button>
+                <button className='butt'>Growing SaaS</button>
+                <button className='butt'>Building Brand</button>
+                <button className='butt ms-5'>Human Res..</button>
+                <button className='butt'>Operations</button>
+                <button className='butt'>Entrepreneur</button>
+                <button className='butt'>Search More</button>
+            </div>
+            <div className='third ms-5'>
+                <h4 className='mt-5 ms-5'>Unlock achievements and rise up the ranks with our <h4 className='text-danger'>reward program.</h4></h4>
+                <p className='ms-5 mt-2'>Our Members inspire us to build this community of people to connect the right mentor with the <br />right mentee, hear what they have to say.</p>
+            </div>
+            <div className='float'>
+                <h4>Empowering students with information-explore ongoing <h4 className='text-danger'>Governmet schemes</h4></h4>
+                <p>Our comprehensive guide to government-funded student schemes <br />will help you access the support you need to achieve your goals.</p>
+            </div>
+            <div className='container mt-5'>
+                <h1 className='d-flex text-center'><b>From Skeptic to Success-</b><h1 className='dev'>Hear it from them</h1></h1>
+                <p className='text-center'>Our Members inspire us to build this community of people to connect the right mentor with the <br />right mentee, hear what they have to say.</p>
+            </div>
+
+            <div className='d-flex mt-5'>
+                <div className='card h-100 w-25 bord'>
+                    <div className='d-flex'>
+                        <img src="https://media.istockphoto.com/id/1365601848/photo/portrait-of-a-young-woman-carrying-her-schoolbooks-outside-at-college.jpg?b=1&s=170667a&w=0&k=20&c=eHoKjGFqJQaWsr6dSEGSwwh6dh8SOhN7_RyZm0f3D2o=" height="50" width="60" className='mt-2 ms-5' alt="" />
+                        <p className='mt-3 ms-3'>Anushka, CS student</p>
+                    </div>
+                    <p className='ms-3'>Get this image on: istockphoto.com | License<br /> detailsCreator: PeopleImages Credit: Getty Images/iStockphoto<br /> Want to know where this information comes from? Learn more</p>
+                </div>
+
+                <div className='card h-100 w-25 bord ms-5'>
+                    <div className='d-flex'>
+                        <img src="https://media.istockphoto.com/id/1365601848/photo/portrait-of-a-young-woman-carrying-her-schoolbooks-outside-at-college.jpg?b=1&s=170667a&w=0&k=20&c=eHoKjGFqJQaWsr6dSEGSwwh6dh8SOhN7_RyZm0f3D2o=" height="50" width="60" className='mt-2 ms-5' alt="" />
+                        <p className='mt-3 ms-3'>Anushka, CS student</p>
+                    </div>
+                    <p className='ms-3'>Get this image on: istockphoto.com | License<br /> detailsCreator: PeopleImages Credit: Getty Images/iStockphoto<br /> Want to know where this information comes from? Learn more</p>
+                </div>
+
+                <div className='card h-100 w-25 bord ms-5'>
+                    <div className='d-flex'>
+                        <img src="https://media.istockphoto.com/id/1365601848/photo/portrait-of-a-young-woman-carrying-her-schoolbooks-outside-at-college.jpg?b=1&s=170667a&w=0&k=20&c=eHoKjGFqJQaWsr6dSEGSwwh6dh8SOhN7_RyZm0f3D2o=" height="50" width="60" className='mt-2 ms-5' alt="" />
+                        <p className='mt-3 ms-3'>Anushka, CS student</p>
+                    </div>
+                    <p className='ms-3'>Get this image on: istockphoto.com | License<br /> detailsCreator: PeopleImages Credit: Getty Images/iStockphoto<br /> Want to know where this information comes from? Learn more</p>
+                </div>
+
+                <div className='card h-100 w-25 bord ms-5'>
+                    <div className='d-flex'>
+                        <img src="https://media.istockphoto.com/id/1365601848/photo/portrait-of-a-young-woman-carrying-her-schoolbooks-outside-at-college.jpg?b=1&s=170667a&w=0&k=20&c=eHoKjGFqJQaWsr6dSEGSwwh6dh8SOhN7_RyZm0f3D2o=" height="50" width="60" className='mt-2 ms-5' alt="" />
+                        <p className='mt-3 ms-3'>Anushka, CS student</p>
+                    </div>
+                    <p className='ms-3'>Get this image on: istockphoto.com | License<br /> detailsCreator: PeopleImages Credit: Getty Images/iStockphoto<br /> Want to know where this information comes from? Learn more</p>
+                </div>
+            </div>
+
+            <h3 className='fifth text-center'>
+                "Level up your career with our mentorship platform <br />- where  inspiration meets achievement. Join now<br /> and let's make your dreams a reality!"
+            </h3>
+
+            <nav class="navbar navbar-expand-sm shadow-lg bg-light navbar-dark bg-white">
+                <div className='d-flex'>
+                    <img src="https://media.licdn.com/dms/image/D560BAQEQ-KGYhPajJQ/company-logo_200_200/0/1666896719984?e=1687392000&v=beta&t=ahggY_4x-h4jXPIdU5OJ42n96L8IXjRq5Am5tNhY0DQ" className='ms-5 mt-3' height="37" width="40" alt="" />
+                    <h4 className='text-primary mt-3'>Grow</h4>
+                    <h4 className='mt-3'>binar.</h4><br />
+                </div>
+                <div className='mt-5'>
+                    <p className='mt-5 text-center me-5'>Grwbinar is a mentorship platform to efficiently connect Mentors which enjoy helping people mentees mentees.</p>
+                </div>
+                <div class="d-flex mx-auto mt-3">
+                    <p class="text-primary d-flex">Become a mentor</p>
+                    <p class="text-primary ms-5">Careers</p>
+                    <p class="text-primary ms-5">About</p>
+                </div>
+                <div className='d-flex list-inline'>
+                    <li className='ms-3'>Contact Us</li>
+                    <li className='ms-3'>Privacy Policy</li>
+                    <li className='ms-3 me-5'>Terms Of Use</li>
+                </div>
+            </nav>
+        </div>
+    )
+}
